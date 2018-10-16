26158db9acafbd3a88ab66a645a71bb29a37d1f7
Micha Gorelick <mynameisfiber@gmail.com>
2018-10-16 17:34:10 -0400

    rehauled stripe checkout system to get ready for google sheets integration

--- _webtask/stripe-payment.js
+++ _webtask/stripe-payment.js
@@ -1,24 +1,54 @@
 'use latest';
 
+require("babel-polyfill");
 import express from 'express';
-import { fromExpress } from 'webtask-tools';
 import bodyParser from 'body-parser';
 import stripe from 'stripe';
+import GoogleSpreadsheet from 'google-spreadsheet';
+import { promisify } from 'util';
+import { fromExpress } from 'webtask-tools';
 
-var app = express();
+const app = express();
 app.use(bodyParser.urlencoded({
   extended: true,
 }));
 
+
+const asyncMiddleware = fn =>
+    (req, res, next) => {
+        Promise.resolve(fn(req, res, next))
+            .catch((e) => {
+                console.log(e);
+                return error_response({
+                    res: res,
+                    e: e,
+                })
+            });
+    };
+
+
+function error_response(cfg) {
+    let {res, msg, e, code} = cfg;
+    code = code || 500;
+    e = e || '';
+    msg = msg || "Something went wrong, please try again or contact the website owner";
+    return res.json({
+        statusCode: code,
+        message: msg,
+        debug: "" + e,
+    })
+}
+
+
 function base_preprocess(args) {
     try {
-        var meta = JSON.parse(args.stripeMetadata || '{}');
+        let meta = JSON.parse(args.stripeMetadata || '{}');
     } catch(e) {
-        res.json({
-            statusCode: 400,
-            message: "Invalid request, please try again or contact the website owner: " + e,
+        return error_response({
+            res: res,
+            e: e,
+            code: 400,
         })
-        return null
     }
     return {
         reocurring: (args.reocurring == 'true'),
@@ -31,11 +61,11 @@ function base_preprocess(args) {
     };
 }
 
-var product_preprocess = {};
+let product_preprocess = {};
 
 product_preprocess['prod_ChLIFCpTiz59TQ'] = function(res, args) {
-    var params = base_preprocess(args);
-    var meta = Object.assign(params.meta, args.metadata);
+    let params = base_preprocess(args);
+    let meta = Object.assign(params.meta, args.metadata);
     if (meta.tshirt_size == 'false') {
         meta.tshirt_size = null;
     }
@@ -43,11 +73,11 @@ product_preprocess['prod_ChLIFCpTiz59TQ'] = function(res, args) {
 
     if ((!params.reocurring &&    params.amount < 5000) || 
         ( params.reocurring && 12*params.amount < 5000)) {
-        res.json({
-            statusCode: 400,
-            message: 'You must donate more than $50 annually to become a member'
+        return error_response({
+            res: res,
+            msg: 'You must donate more than $50 annually to become a member',
+            code: 400,
         });
-        return null;
     }
     return params;
 }
@@ -56,84 +86,93 @@ product_preprocess['prod_ChLH0CqLNzANNR'] = function(res, args) {
     return base_preprocess(args);
 }
 
-function find_or_create_customer(_stripe, params) {
-    return _stripe.customers.list({
+async function find_or_create_customer(_stripe, params) {
+    let customers = await _stripe.customers.list({
         email: params.email
-    }).then(customers => {
-        if (customers.data.length) {
-            var customer = customers.data[0];
-            var meta = Object.assign(params.meta, customer.metadata);
-            return _stripe.customers.update(customer.id, {
-                source: params.source,
-                metadata: meta
-            });
-        } else {
-            return _stripe.customers.create({
-                email: params.email,
-                source: params.source,
-                metadata: params.meta,
-            });
-        }
-    })
+    });
+
+    if (customers.data.length) {
+        let customer = customers.data[0];
+        let meta = Object.assign(params.meta, customer.metadata);
+        return _stripe.customers.update(customer.id, {
+            source: params.source,
+            metadata: meta
+        });
+    } else {
+        return _stripe.customers.create({
+            email: params.email,
+            source: params.source,
+            metadata: params.meta,
+        });
+    }
 }
 
+app.get('/', asyncMiddleware(async (req, res) => {
+    let ctx = req.webtaskContext;
+    let doc = new GoogleSpreadsheet('1z9f59uFfyaZMKfXdAACdJY9bY4RZZ_2FuHqkwgzX7R8');
+    await promisify(doc.useServiceAccountAuth)({
+        client_email: 'taskrunner@membership-data-tracker.iam.gserviceaccount.com',
+        private_key: ctx.secrets.GDRIVE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    });
+    let info = await promisify(doc.getInfo)();
+    let worksheet = info.worksheets[0];
+    let rows = await promisify(worksheet.getRows)()
+    console.log(rows);
+    return res.json({
+        code: 200,
+        msg: 'ELO',
+        info: rows,
+    })
+}))
 
-app.post('/payment',    (req,res) => {
-    var ctx = req.webtaskContext;
-    var STRIPE_SECRET_KEY = ctx.secrets.STRIPE_SECRET_KEY;
-    var args = req.body;
-    var params = product_preprocess[req.body.productId](res, req.body)
+app.post('/payment', asyncMiddleware(async (req, res) => {
+    let ctx = req.webtaskContext;
+    let STRIPE_SECRET_KEY = ctx.secrets.STRIPE_SECRET_KEY;
+    let args = req.body;
+    try {
+        let params = product_preprocess[req.body.productId](res, req.body)
+    } catch(e) {
+        return error_response({
+            res: res,
+            msg: 'Invalid product',
+            e: e,
+            code: 400,
+        });
+    }
     if (params === null) { return }
 
-    var _stripe = stripe(STRIPE_SECRET_KEY);
-    find_or_create_customer(_stripe, {
+    let _stripe = stripe(STRIPE_SECRET_KEY);
+    let customer = await find_or_create_customer(_stripe, {
         email: params.email,
         source: params.source,
         meta: params.meta,
-    }).catch(e => {
-        res.json(e);
-        return Promise.reject(e);
-    }).then(customer => {
-        if (customer === null) { return null; }
-        if (params.reocurring) {
-            _stripe.plans.create({
-                product: params.productId,
-                currency: 'usd',
-                interval: 'month',
-                nickname: params.description,
-                amount: params.amount,
-            }).catch(e => {
-                res.json(e);
-                return Promise.reject(e);
-            }).then(plan => {
-                if (plan === null) { return null; }
-                _stripe.subscriptions.create({
-                    customer: customer.id,
-                    items: [{plan: plan.id}],
-                }).catch(e => {
-                    res.json(e);
-                    return Promise.reject(e);
-                }).then(subscription => {
-                    if (subscription === null) { return null; }
-                    res.json(subscription);
-                    return 
-                });
-            });
-        } else {
-            _stripe.charges.create({
-                amount: params.amount,
-                currency: 'usd',
-                customer: customer.id,
-                description: params.description,
-            }).catch(e => {
-                res.json(e);
-                return Promise.reject(e);
-            }).then(charge => {
-                if (charge === null) { return null; }
-                return res.json(charge);
-            });
-        }
-    });
-});
+    })
+    if (customer === null) { throw "Could not create customer"; }
+    if (params.reocurring) {
+        let plan = await _stripe.plans.create({
+            product: params.productId,
+            currency: 'usd',
+            interval: 'month',
+            nickname: params.description,
+            amount: params.amount,
+        });
+        if (plan === null) { throw "Could not create plan"; }
+        let subscription = await _stripe.subscriptions.create({
+            customer: customer.id,
+            items: [{plan: plan.id}],
+        })
+        if (subscription === null) { throw "Could not create subscription"; }
+        return res.json(subscription);
+    } else {
+        let charge = await _stripe.charges.create({
+            amount: params.amount,
+            currency: 'usd',
+            customer: customer.id,
+            description: params.description,
+        })
+        if (charge === null) { throw "Could not create charge"; }
+        return res.json(charge);
+    }
+}));
 
 module.exports = fromExpress(app);
